018076e9617025cb3cbbc80c949f20fa0c338a37
Jason Burke <jpburke77@gmail.com>
2025-03-14 04:01:40 -0500

    chore: complete error msg style and functions

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const successForm = document.getElementById('success');
 const form = document.getElementById('form');
 const dismissBtn = document.getElementById('dismiss-btn');
 
-// TODO create a validation function. return t/f
 function validateEmail(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
@@ -16,27 +15,30 @@ function toggleDisplay(element) {
     : (element.style.display = 'none');
 }
 
-// console.log(successForm.lastElementChild);
-
-form.addEventListener('submit', function (event) {
-  event.preventDefault();
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
   const formData = new FormData(form);
   const data = Object.fromEntries(formData.entries());
+  const emailInput = document.getElementById('email');
+  const errorMsgContainer =
+    document.getElementById('label-container').lastElementChild;
 
   if (validateEmail(data.email)) {
     toggleDisplay(signupForm);
     toggleDisplay(successForm);
     localStorage.setItem('email', data.email);
     document.getElementById('user-email').textContent = data.email;
+    errorMsgContainer.style.display = 'none';
+    emailInput.style.borderColor = 'black';
+    emailInput.style.backgroundColor = 'hsl(0, 0%, 100%)';
+    emailInput.style.color = 'black';
+
     form.reset();
   } else {
-    const emailInput = document.getElementById('email');
-    const labelContainer =
-      document.getElementById('label-container').lastElementChild;
+    errorMsgContainer.style.display = 'block';
     emailInput.style.borderColor = 'hsl(4, 100%, 67%)';
     emailInput.style.backgroundColor = 'hsla(4, 100%, 67%, 0.158)';
     emailInput.style.color = 'hsl(4, 100%, 67%)';
-    labelContainer.style.display = 'block';
   }
 });
 
